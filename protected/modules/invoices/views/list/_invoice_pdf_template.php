d5c113459d8933ca03de77be6dc8eee754d8a6ca
darkoffalex <darkoffalex@yandex.ru>
2014-06-09 18:06:02 +0300

    pdf
    Added pdf generation function

---
+++ protected/modules/invoices/views/list/_invoice_pdf_template.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<table cellpadding="0" cellspacing="0" class="header-tbl">
+    <tr>
+        <td style="text-align: right; font-size: 16px;" colspan="3">PVM SASAKITA FAKTURA<br><br></td>
+    </tr>
+    <tr>
+        <td style="font-size: 15px;">Pardavejas:</td>
+        <td><strong>UAB Vandens filtravimo sistemos</strong><br>Imones kodas:2568746545; PVM kodas: LT583500515<br><br><span class="sm-1">Silutes pl.4 Klaipeda.<br> Dnb Nord bankas LT2440125641898</span></td>
+        <td style="text-align: right;"><strong>Serija VFSV Nr. 1123</strong><br><br>Data 2014.05.19</td>
+    </tr>
+</table>
+<br><br>
+<hr class="hr">
+<br><br>
+<table cellpadding="0" cellspacing="0" class="customer-tbl">
+    <tr>
+        <td style="font-size: 15px;">Pirkejas:</td>
+        <td><strong>Jozas Baskys<br>a.k. 34845648964</strong><br>Alytaus raj. Butrimonys Margirio g. 15</td>
+        <td>Telefonas, atsakingas<br>862725514</td>
+    </tr>
+</table>
+<br><br>
+<table cellpadding="0" cellspacing="0" class="invoice-data-tbl">
+    <tr class="head-tbl">
+        <td style="height: 20px;">Pavadinimas</td>
+        <td style="height: 20px;">Mato Vnt.</td>
+        <td style="height: 20px;">Kiekis</td>
+        <td style="height: 20px;">Kaina Lt.</td>
+        <td style="height: 20px;">Suma Lt.</td>
+    </tr>
+
+    <tr>
+        <td>Filtras takoi-to tra-par-tar 58OL</td>
+        <td>vnt.</td>
+        <td>36</td>
+        <td>25.0000</td>
+        <td>900.00</td>
+    </tr>
+
+    <tr>
+        <td>Filtras takoi-to tra-par-tar 58OL</td>
+        <td>vnt.</td>
+        <td>36</td>
+        <td>25.0000</td>
+        <td>900.00</td>
+    </tr>
+
+    <tr>
+        <td>Filtras takoi-to tra-par-tar 58OL</td>
+        <td>vnt.</td>
+        <td>36</td>
+        <td>25.0000</td>
+        <td>900.00</td>
+    </tr>
+
+    <tr>
+        <td>Apmoketi pavedimui iki 2014-06-15</td>
+        <td></td>
+        <td></td>
+        <td>0.0000</td>
+        <td>0.00</td>
+    </tr>
+
+    <tr>
+        <td style="border: none;">Is viso:</td>
+        <td style="border: none;"></td>
+        <td style="border: none;">98</td>
+        <td style="border: none;"><span class="no-wrap">Suma be PVM</span><br><span class="no-wrap">PVM suma (21%)</span></td>
+        <td>5020.00<br>1054.20</td>
+    </tr>
+
+    <tr>
+        <td style="border: none;"></td>
+        <td style="border: none;"></td>
+        <td style="border: none;"></td>
+        <td style="border: none;">Suma su PVM</td>
+        <td>6074.20</td>
+    </tr>
+
+</table>
+<p><strong>Suma zodziais: Sesi tukstanciai septinesdesimt-keturi litai 20 centu</strong></p>
+<p class="sized-p">Neapmokejus per nurodyta termina uz kiek-viena-diena priskaiciuoja 0.2% delspenigu. </p>
+<p>Saskaita israse: Laura Stukaite</p>
+<p>Saskaita gavo:</p>
+</body>
+</html>
