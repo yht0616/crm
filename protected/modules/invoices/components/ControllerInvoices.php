3d3ca804ba83a562770c7dec30988c5e495e9c69
ruslankus <ruslankus@yahoo.com>
2014-06-09 13:47:38 +0300

    Заменменили путь к лайауту

--- protected/modules/invoices/components/ControllerInvoices.php
+++ protected/modules/invoices/components/ControllerInvoices.php
@@ -3,12 +3,9 @@
 class ControllerInvoices extends CController
 {
 
-    public $layout= '/layouts/main_layout';
+    public $layout= 'main_layout';
 
-    public function __construct($id,$module=null){
-
-        parent::__construct($id,$module);
-    }
+    
 
     protected function beforeAction($action) {
 
