2023c90635aa2a175b40cbf19541922fbe0881cf
darkoffalex <darkoffalex@yandex.ru>
2014-06-09 13:54:30 +0300

    resolving conflict
    resolving conflict

--- protected/modules/invoices/components/ControllerInvoices.php
+++ protected/modules/invoices/components/ControllerInvoices.php
@@ -2,15 +2,6 @@
 
 class ControllerInvoices extends CController
 {
-<<<<<<< HEAD
-    public $layout= '/layouts/main_layout';
-=======
-
-    public $layout= 'main_layout';
->>>>>>> 3d3ca804ba83a562770c7dec30988c5e495e9c69
-
-    
-
     protected function beforeAction($action) {
 
         return parent::beforeAction($action);
