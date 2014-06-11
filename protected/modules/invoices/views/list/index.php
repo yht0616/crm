0078c094e6915947fb1ca88197ff29586c42b7bf
ruslankus <ruslankus@yahoo.com>
2014-06-11 15:10:40 +0300

    исправленна база

--- protected/modules/invoices/views/list/index.php
+++ protected/modules/invoices/views/list/index.php
@@ -60,19 +60,7 @@
                 <div class="modal fade"  id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                     <div class="modal-dialog">
                         <div class="modal-content">
-                            <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                <h4 class="modal-title">Modal title</h4>
-                            </div><!--/modal-header -->
-
-                            <div class="modal-body">
-                                <p>One fine body&hellip;</p>
-                            </div><!--/modal-body -->
-
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
-                            </div><!--/modal-footer -->
+                            
 
                         </div><!-- /.modal-content -->
                     </div><!-- /.modal-dialog -->
