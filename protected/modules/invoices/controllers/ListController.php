3789633f85add45213f2ab1a9aa7327f69269bdc
darkoffalex <darkoffalex@yandex.ru>
2014-06-11 12:50:49 +0300

    preporations for pdf generation

--- protected/modules/invoices/controllers/ListController.php
+++ protected/modules/invoices/controllers/ListController.php
@@ -7,14 +7,38 @@ class ListController extends ControllerInvoices
         $this->render('list_invoices');
     }
 
-    public function actionPdf()
+    public function actionGetPdf()
+    {
+        $id = Yii::app()->request->getParam('id');
+        debugvar($id);
+    }
+
+    public function actionGenPdf()
+    {
+        //get id from request
+        $id = Yii::app()->request->getParam('id');
+
+        //find invoice from base
+        $invoice = Invoices::model->findByPk($id);
+
+        //if invoice found
+        if($invoice != null)
+        {
+            //generate pdf
+            $pdf_filename = $this->GeneratePdf($invoice);
+
+            
+        }
+    }
+
+    private function GeneratePdf($invoice)
     {
         //include mDpf libs
         $mPdf_dir=dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'mpdf/mpdf.php';
         require_once($mPdf_dir);
 
         //get html for pdf from partial
-        $html = $this->renderPartial('_invoice_pdf_template',array(),true);
+        $html = $this->renderPartial('_invoice_pdf_template',array('invoice' => $invoice),true);
 
         //create new pdf
         $pdf = new mPDF('utf-8', 'A4', '8', 'Arial', 10, 10, 10, 10, 10, 10);
@@ -36,15 +60,28 @@ class ListController extends ControllerInvoices
         }
 
         //filename
-        $file_name = 'generated.pdf';
+        $file_name = md5($invoice->id).".pdf";
 
         //if dir created
         if(file_exists('pdf'))
         {
             //save file
             $pdf->Output('pdf/'.$file_name, 'F');
+
+            //return filename
+            return $file_name;
         }
 
+        //if dir not found
+        else
+        {
+            //return nothing
+            return "";
+        }
+
+
+
+        /*
         //return file to download
         if(file_exists('pdf/'.$file_name))
         {
@@ -57,7 +94,7 @@ class ListController extends ControllerInvoices
         {
             exit('File not found');
         }
-
+        */
     }
 
     public function actionIndex()  {
@@ -74,7 +111,7 @@ class ListController extends ControllerInvoices
     public function actionOps($id){
         
         $request = Yii::app()->request;
-        
+
         if($request->isAjaxRequest){        
             $objOps = Ops::model()->with('users')->findByPk($id);
             $listGoods = Listgoods::model()->findAllByAttributes(array('ops_id'=>$id));        
